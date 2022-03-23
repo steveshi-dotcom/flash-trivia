e415e3bfd908629cb49789c6ffe47486128ce60b
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-03-23 15:32:12 -0400

    desperate attemp to fix routing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import HomePage from "./components/Start/HomePage";
 import Main from './components/Main/Main';
 import Result from './components/Result/Result';
 
+// Urls routes
+export const homeUrl = "/home";
+export const mainUrl = "/game";
+export const resultUrl = "/result";
+
 function App() {    /*TODO Resize the elements so that the view on the phone would have the video ono the bottom */
   // http://localhost:3000/flash-trivia
+
   return (
     <div className="App">
       <Router>
           <Routes>
-            <Route path={"/flash-trivia/HomePage"} element={<HomePage />}/>
-            <Route path={"/flash-trivia/GamePage"} element={<Main userDifficulty={"hard"}/>}/>
-            <Route path={"/flash-trivia/ResultPage"} element={<Result />}/>
+            <Route path={homeUrl} element={<HomePage />}/>
+            <Route path={mainUrl} element={<Main userDifficulty={"hard"}/>}/>
+            <Route path={resultUrl} element={<Result />}/>
           </Routes>
       </Router>
     </div>
