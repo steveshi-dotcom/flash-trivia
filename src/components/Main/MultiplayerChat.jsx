fb60f834ea0d7d9ed15c056c6c57a414742df7ce
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-03-23 17:16:21 -0400

    save result before attempting to download express

--- src/components/Main/MultiplayerChat.jsx
+++ src/components/Main/MultiplayerChat.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
+const express = require('express');
+const app = express();
 
+app.get('./', (req, res) => {
+  res.send('Hello from Steve');
+});
+app.listen(1234, () => console.log("Connected to the server"));
 
 // Chat part of Multiplayer component where the user will be able to communicate with four other player
 const MultiplayerChat = () => {
