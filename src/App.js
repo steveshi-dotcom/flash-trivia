5cd0ae80c07f3e12a6b4547aad03d20369d617ec
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-03-23 15:24:44 -0400

    fix react routing for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import HomePage from "./components/Start/HomePage";
 import Main from './components/Main/Main';
@@ -8,15 +8,15 @@ import Result from './components/Result/Result';
 function App() {    /*TODO Resize the elements so that the view on the phone would have the video ono the bottom */
   // http://localhost:3000/flash-trivia
   return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path={"flash-trivia/HomePage"} element={<HomePage />}/>
-          <Route path={"flash-trivia/GamePage"} element={<Main userDifficulty={"hard"}/>}/>
-          <Route path={"flash-trivia/ResultPage"} element={<Result />}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Router>
+          <Routes>
+            <Route path={"/flash-trivia/HomePage"} element={<HomePage />}/>
+            <Route path={"/flash-trivia/GamePage"} element={<Main userDifficulty={"hard"}/>}/>
+            <Route path={"/flash-trivia/ResultPage"} element={<Result />}/>
+          </Routes>
+      </Router>
+    </div>
     /*<div className={"App"}>
       <Main userDifficulty={"hard"}/>
     </div>*/
