7c6e3de681dc627bc21f8ce315ef17424a72c309
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-03-27 18:41:55 -0400

    put up score that was read from url, need to implement answer board

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useLocation } from "react-router-dom";
+import styled from 'styled-components';
+import qs from 'qs';
+import { searchScoreParam } from "../Main/TriviaGame.jsx";
+import {searchNameParam, searchRoomParam} from "../Start/HomePage.jsx";
+import Answer from "../Main/TriviaGame/Answer.jsx";
+
+// styled components
+const ScoreBoard = styled.h1`
+  font-size: 3.5em;
+  font-weight: bold;
+`
+const AnswerBoard = styled.div`
+  display: flex;
+  background: white;
+  justify-content: center;
+  align-items: center;
+  width: 10vw;
+`
+const InvidualAnswers = styled.p`
+
+`
 
 const Result = () => {
+  const playerLocation = useLocation();
+  const [displayScore, setDisplayScore] = useState(0);
+
+  useEffect(() => {
+    const scoreInput = qs.parse(playerLocation.search.slice(1)); // playerLocation.search: ?name=steve&room=1234
+    setDisplayScore(scoreInput[searchScoreParam]);
+  }, [playerLocation]);
+
   return(
-    <div>
-      Ending ==> How'd u do?
+    <div style={{alignItems: "center", justifyContent: "center"}}>
+      <ScoreBoard>Score: {displayScore}%</ScoreBoard>
+      <AnswerBoard>
+        <InvidualAnswers>asfd</InvidualAnswers>
+      </AnswerBoard>
     </div>
   )
 }
