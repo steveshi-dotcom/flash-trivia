adc72ea9a06da827911070d9759cd456c174976e
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-03-28 09:50:10 -0400

    reorganize project structure

---
+++ client/src/components/Main/TriviaGame.jsx
@@ -0,0 +1,132 @@
+import React, {useState, useEffect, useRef} from 'react';
+import styled from 'styled-components';
+import GameLoading from "./TriviaGame/GameLoading.jsx";
+import Question from './TriviaGame/Question.jsx';
+import Answer from './TriviaGame/Answer.jsx';
+import rightAnswer from './imgs/rightMeme.jpeg';
+import wrongAnswer from './imgs/wrongMeme.jpeg';
+import {createSearchParams, useNavigate} from "react-router-dom";
+import {resultUrl} from "../../App.js";
+
+// Properties of each question from OpenTrivia API
+const tResponse = 'response_code';  // Response of API request
+const tType = 'type';
+const tQuestion = 'question';
+const tCorrect = 'correct_answer';
+const tIncorrect = 'incorrect_answers';
+
+// Styled Components
+export const TriviaGameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 60%;
+  height: 99%;
+  padding: 0 0 5% 0;
+`
+
+export const searchScoreParam = 'score';
+
+// Question Queue to track the question when the user starts playing the game.
+const TriviaGame = (props) => {
+  // navigation function use to navigate through pages
+  const navigate = useNavigate();
+
+  // States of the TriviaGame Function
+  const [loading, setLoading] = useState(true);
+  const [userDifficulty, setUserDifficulty] = useState(props.userDifficulty);
+  const [triviaRound, setTriviaRound] = useState(0);
+  const [triviaQueue, setTriviaQueue] = useState([]);
+  const [questionNum, setQuestionNum] = useState(0);
+  const [userPoints, setUserPoints] = useState(0);
+  const [memeTime, setMemeTime] = useState({"display": false});
+  const runTimer = useRef(0);
+
+  // Async function to return an array of 50 trivia question from OpenTrivia API, a new batch of question every call
+  const getTriviaQuestion = async() => {
+    const openTrivia_call = await fetch(`https://opentdb.com/api.php?amount=50&difficulty=${userDifficulty}&type=multiple`)
+    const openTrivia_data = await openTrivia_call.json();
+    if (openTrivia_data[tResponse] !== 0) { // When the api_call failed to send any question send an error
+      alert("Oops 401 Error occurred, please try again later.");
+    }
+    await setTriviaQueue(openTrivia_data.results);
+
+    // Load up the rendering of the question after triviaQueues finish up fetching the question
+    setTimeout(() => {
+      setLoading(false);
+    }, 1); // Allow the user to look at the puppy pic a bit longer :)
+  }
+
+  // New round of trivia game, only started on the first round or every new round
+  useEffect(() => {
+    setUserDifficulty(props.userDifficulty);
+    getTriviaQuestion().then(res => console.log("BEEP BEEP BOP, Finished loading up the questions."));
+    setQuestionNum(0);
+    setUserPoints(0);
+    runTimer.current = 0;
+    // eslint-disable-next-line
+  }, [triviaRound]);
+
+  // Add up points for the user, and move on to the next question, go to results when finished
+  const handlePoints = (e) => {
+    if (e === 1) {
+      setUserPoints(userPoints + 2);
+      setMemeTime({"display": true, "type": 1, "unknownMeme": sendUserTheirGift(1)})
+    } else {
+      setMemeTime({"display": true, "type": 0,  "unknownMeme": sendUserTheirGift(0)})
+    }
+    // Consider whether to move on to the next question or that finish up the game when it reaches the maximum
+    if (questionNum < 49) {
+      setQuestionNum(questionNum + 1);
+    } else {
+      finishCurrentRound();
+    }
+  }
+
+  // Send the user a meme of the result of what they picked (right/wrong), option to move on or a timer??
+  const sendUserTheirGift = async(e) => {
+    if (e === 1) {
+      return <img src={rightAnswer} alt={"right-answer"} />;
+    } else {
+      return <img src={wrongAnswer} alt={"wrong-answer"} />
+    }
+  }
+
+  // Finish the current round and move on to the result page
+  // where the user can either laugh about the good game 🙂
+  // or cry on their stupidity😞
+  const finishCurrentRound = () => {
+    setTriviaRound(triviaRound + 1);
+    navigate({
+      pathname: resultUrl,
+      search: createSearchParams([
+        [searchScoreParam, userPoints]
+      ]).toString()
+    });
+  }
+
+  // Render Trivia Game (Left Section of Main)
+  return(
+    <TriviaGameContainer>
+      {!loading ? (
+        <div>
+          <Question qNum={questionNum}
+                    qType={triviaQueue[questionNum][tType]}
+                    qQues={triviaQueue[questionNum][tQuestion]}
+                    qResults={(e) => handlePoints(e)}
+                    qRunTimer={runTimer}
+          />
+          <Answer qCorrect={triviaQueue[questionNum][tCorrect]}
+                  qIncorrect={triviaQueue[questionNum][tIncorrect]}
+                  qResult={(e) => handlePoints(e)}
+          />
+        </div>
+      ) : (
+        <GameLoading />
+      )}
+    </TriviaGameContainer>
+  )
+}
+export default TriviaGame;
---
+++ client/src/components/Main/TriviaMultiplayer/MultiCommunication.jsx
@@ -0,0 +1,262 @@
+import React, {useState, useEffect, useRef} from 'react';
+import styled from 'styled-components';
+import { useLocation } from "react-router-dom";
+import qs from 'qs';
+import { searchNameParam, searchRoomParam } from "../../Start/HomePage.jsx";
+import { v4 as uuidv4 } from 'uuid';
+import Peer from 'peerjs';
+import socketIOClient from 'socket.io-client';
+
+import Video from "./Video.jsx";
+
+export const socket = socketIOClient(`http://localhost:3001`, {secure: false});
+
+// ----styled components----
+/** CHAT */
+const ChatRootContainer = styled.div` // Root of the Chat rendering where clients send msg to communicate with others
+  background-color: black;
+  color: black;
+  font-size: 1em;
+  font-family: Arial,serif;
+  display: flex;
+  flex-direction: column;
+  border-radius: .25em;
+  margin-top: 1vh;
+`
+const ChatHistoryContainer = styled.div` // History of past chat
+  width: 40.33vw;
+  height: 28.5vh;
+  margin-right: auto;
+  padding: 3px 0 0 3px;
+  overflow-x: hidden;
+  overflow-y: auto;
+`
+// Individual Message from each player
+const IndividualChat = styled.p` // Individual chat style components
+  display: flex;
+  width: 40.33vw;
+  margin: .75vh 0 5px .5vw;
+  color: white;
+`
+const IndividualChatFirstName = styled.span`
+  font-weight: bold;
+  display: flex;
+  width: 2.5vw;
+  margin-right: 1.5vw;
+  text-align: left;
+`
+const IndividualChatMsg = styled.span`
+  text-align: left;
+  margin-right: 2vw;
+  word-spacing: 0.75px;
+`
+// Box to insert new message to the group
+const InsertChatContainer = styled.div` // Container where user can send a message and post it inside ChatHistory
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const InsertChatInput = styled.input` // Text-input to type up chat
+  background-color: white;
+  width: 38.25vw;
+  height: 4vh;
+  font-size: 1.5em;
+  margin: 0;
+`
+const InsertChatBtn = styled.button` // Send a event to add chat to ChatHistoryContainer
+  cursor: pointer;
+  width: 2vw;
+  height: 4.35vh;
+  background-color: deepskyblue;
+  border: none;
+  margin: 0;
+  :hover {
+    font-weight: bold;
+  }
+  :active {
+    background-color: limegreen;
+  }
+`
+
+/** VIDEO */
+const VideoRootContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+
+// MultiCommunication part of the Main where player can communicate with four other player via video/chat
+const MultiCommunication = () => {
+  // unique uuid for each player, use for keeping track of users in socket for chat and peer identification for video
+  const [userId, setUserId] = useState(`${uuidv4()}`);
+
+  // const representing value of the three properties that will be emitted to the socket server
+  const [userName, setUserName] = useState('');
+  const [userRoom, setUserRoom] = useState('');
+  const userMsg = useRef('');
+
+  // chatHistory: container for chat history among 5 players
+  // each index stores an object with just userName + userMsg prop
+  const [chatHistory, setChatHistory] = useState([]);
+
+  // Update the chatHistory by setting the new state as the copy of the history with incomingMsg appended to it
+  const updateChatHistory = (incomingMsg) => {
+    const chatHistoryCopy = chatHistory;
+    chatHistoryCopy.push(incomingMsg);
+    setChatHistory([...chatHistoryCopy]);
+  }
+
+  // () Obtain the player information from the url such as: http://localhost:3000/game/?name=steve&room=1234
+  // Effect() After a player joined, get their information and inform other players in the chat room
+  const playerLocation = useLocation();
+  useEffect(() => {
+    const playerInput = qs.parse(playerLocation.search.slice(1)); // playerLocation.search: ?name=steve&room=1234
+    setUserName(playerInput[searchNameParam]);
+    setUserRoom(playerInput[searchRoomParam]);
+
+    // emit an 'join-game' event that will place the player in their room with their team
+    socket.emit("join-game", {
+      userId: userId,
+      userName: playerInput[searchNameParam],
+      userRoom: playerInput[searchRoomParam],
+      userMsg: `I have joined the game at 
+        ${new Date().getHours()}:${new Date().getMinutes() < 10 ?
+        '0' + new Date().getMinutes()
+        : new Date().getMinutes()}
+        `
+    });
+
+    // Listen for 'new-player' event signifying a new player joining their game and send an update to chatHistory
+    socket.on("new-player", (newPlayerData) => {
+      console.log("Informing the others that an new player has joined.");
+      updateChatHistory(newPlayerData);
+    });
+
+    socket.on('old-player', (oldPlayerData) => {
+      console.log("Informing the others that an old player has left.");
+      updateChatHistory(oldPlayerData);
+      removeRemoteStream(oldPlayerData["userId"]);
+    })
+  }, [playerLocation]);
+
+  // () post the new chat into the chatHistory and emitting to all players within the room
+  const postNewChat = () => {
+    const postedChat = {
+      userId: userId,
+      userName: userName,
+      userMsg: "(◔_◔)"
+    };
+    if (userMsg.current.length !== 0) {
+      postedChat['userMsg'] = userMsg.current;
+    }
+    socket.emit("chat-message", postedChat);
+    document.getElementById("chatInput").value = '';
+
+    // Listen for any incoming messages from other players
+    socket.on("chat-message", (incomingChat) => {
+      console.log(`I have received an chat event from the server at ${new Date().getMilliseconds()}-->${incomingChat.userMsg}`);
+      //const newIndividualChat = { userName: incomingChat['userName'], userMsg: incomingChat['userMsg'] }
+      updateChatHistory(incomingChat);
+    });
+  }
+
+  // VIDEO PART ---->
+  // C1: When a player joins, send peerInfo to others. Other call this new peer and add incoming streams to remote
+  //     Other will append the incoming stream to remoteStream
+  // C2: When a player joins, call all other peers and add the streams beside caller....
+
+  // CHOOSE C1 <-----
+
+  // Stores remote streams of all other-player
+  const [remoteStreams, setRemoteStreams] = useState({});
+  const videoRef = useRef(undefined);
+  useEffect(() => {
+    // Getting local streams, existing players within room call new player, new player accept incoming call from others
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+      .then(stream => {
+        // Make new peer object with unique uuid and at server localhost:3002/flash-trivia or whatever in the future
+        const peer = new Peer(userId, {
+          host: 'localhost',
+          port: 3002,
+          path: '/flash-trivia'
+        });
+
+        // Check Peer is on, if not it is not connected to the server and dataConnection won't transport mediaStream
+        peer.on('open',id => {
+          //console.log(id);
+        });
+
+        // Listen for meet-up event, then call other peer using the id. Add their responding stream to remoteStream
+        socket.on('meet-up', otherPeerId => {
+          // console.log(`Initiating CALL ==== MyID is => ${peer.id} \n CallerId is => ${otherPeerId}`);
+          const call = peer.call(otherPeerId, stream);
+          call.on('stream', stream => {
+            addRemoteStream(call.peer, stream);
+          })
+        });
+
+        // Accept new peer calls and provide local media stream to them, add their calling stream to remoteStreams
+        peer.on('call', call => {
+          // console.log(`Intercepting CALL ==== MyID is => ${peer.id} \n CallerId is => ${call.peer}`);
+          call.answer(stream);
+          call.on('stream', stream => {
+            addRemoteStream(call.peer, stream);
+          })
+        });
+      })
+      .catch(err => console.log(err));
+  }, [userId]);
+
+  // () remoteStreams adding/removing 4.5
+  const addRemoteStream = (peerId, video) => {
+    const remoteStreamsCopy = remoteStreams;
+    remoteStreamsCopy[peerId] = video;
+    setRemoteStreams(Object.assign({}, remoteStreamsCopy));
+  }
+  const removeRemoteStream = (peerId) => {  // NEVER CALLED: lost-player event was never intercepted from server
+    console.log("Removing STREAM");
+    console.log(peerId);
+    const remoteStreamsCopy = remoteStreams;
+    delete remoteStreamsCopy[peerId];
+    setRemoteStreams(Object.assign({}, remoteStreamsCopy));
+  }
+
+  // Dummy effect to check if all streams are stored. stores playerNum - 1(sender)
+  useEffect(() => {
+    console.log(remoteStreams); // theoretically should store all other streams beside the player within remoteStreams
+  }, [remoteStreams]);
+
+  // Listen for any player leaving the game and then updating the rest of the players in the game room
+  socket.on("lost-player", (incomingUpdate) => {
+    updateChatHistory(incomingUpdate);
+    removeRemoteStream(incomingUpdate['userId']);
+  });
+
+  // Render the right-hand side: video/chat function
+  return(
+    <div>
+      <VideoRootContainer>
+        {Object.keys(remoteStreams).map((curr, ind) => {
+          const currStream = remoteStreams[curr];
+          return <Video stream={currStream} key={ind} />
+        })}
+      </VideoRootContainer>
+      <ChatRootContainer>
+        <ChatHistoryContainer>
+          {chatHistory.map((curr, ind) => {
+            return <IndividualChat key={ind}>
+              <IndividualChatFirstName>{curr['userName']}:</IndividualChatFirstName>
+              <IndividualChatMsg>{curr['userMsg']}</IndividualChatMsg>
+            </IndividualChat>
+          })}
+        </ChatHistoryContainer>
+        <InsertChatContainer>
+          <InsertChatInput id={"chatInput"} type={"text"} onChange={(e) => userMsg.current = e.target.value} />
+          <InsertChatBtn onClick={postNewChat}>post</InsertChatBtn>
+        </InsertChatContainer>
+      </ChatRootContainer>
+    </div>
+  )
+}
+
+export default MultiCommunication;
