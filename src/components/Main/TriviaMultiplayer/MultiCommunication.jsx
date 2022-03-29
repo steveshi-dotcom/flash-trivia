0659718f7c7c608564da0a52b03183c713182181
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-03-29 19:27:18 -0400

    change socket io server

--- src/components/Main/TriviaMultiplayer/MultiCommunication.jsx
+++ src/components/Main/TriviaMultiplayer/MultiCommunication.jsx
@@ -9,7 +9,7 @@ import socketIOClient from 'socket.io-client';
 
 import Video from "./Video.jsx";
 
-export const socket = socketIOClient(`http://localhost:4001`, {secure: false});
+export const socket = socketIOClient(`https://flash-trivia-v1-server.herokuapp.com/`, {secure: false});
 
 // ----styled components----
 /** CHAT */
