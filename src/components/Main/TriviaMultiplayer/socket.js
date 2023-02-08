ce5e6c3516c307328b5b16e5b74b6a9a8f1388e7
steveshi-dotcom <st3v5.s2i@gmail.com>
2023-02-08 13:52:30 -0500

    change socket url

--- src/components/Main/TriviaMultiplayer/socket.js
+++ src/components/Main/TriviaMultiplayer/socket.js
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
 //const socket = socketIOClient(`https://flash-trivia-v1-server.herokuapp.com/`, {secure: false});
-const socket = socketIOClient(`localhost:4000`, {secure: false});
+const socket = socketIOClient(`https://flash-trivia-server2.onrender.com`, {secure: false});
 
 // Export socket as default to make socket available globally
 export default socket;
